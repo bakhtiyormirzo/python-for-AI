87b47f78f432b45a66a704afa5ba73ca23119720
Bakhtiyorjon Mirzajonov <bakhtiyorjon.m@gmail.com>
2022-05-12 16:26:58 +0300

    ordered the repo

---
+++ class/class01.py
@@ -0,0 +1,89 @@
+# Pythonda dasturlash
+
+# Operatorlar
+'''
++
+-
+/
+*
+
+
+// - butunli bolish
+** - daraja
+% - qoldiqli bolish
+'''
+
+'''
+print("1 + 5 = ",1 + 5)
+print("1 - 9 = ",1 - 9)
+print("5 / 2 = ",5 / 2)
+print("5 * 6 = ",5 * 6)
+print("5 // 2 = ",5 // 2)
+print("5 ** 2 = ", 5 ** 2)
+print("0 ** 0 = ", 0 ** 0)
+'''
+
+
+# kommentariya -> # 
+
+
+# Ozgaruvchilar
+'''
+a = 5
+b = 6
+print("a + b = ", a + b)
+'''
+
+# O'zgaruvchilar qoidalar
+# katta/kichik harflar, _, sonlar ishlatsa boladi
+# sonlar bilan boshlanmaydi
+# probel ishlatib bolmaydi
+
+
+# Toifalar
+'''
+int() - butun sonlar - 1 2 3 4 5 6 7 8 9
+float() - haqiqiy sonlar - 1.5535 5.4345 10.2222
+str() - satr - matn
+bool() - mantiqiy toifa
+type() 
+'''
+
+# Funksiyalar
+# print() - chiqarish funksiyasi
+# input() - kiritish funksiyasi
+
+'''
+print('O\'zbekiston')
+
+a = int(input("1-sonni kiriting: "))
+b = int(input("2-sonni kiriting: "))
+print(a + b)
+'''
+
+# Mashqlar
+
+# 1
+# print(int(input("a = ")) ** 2)
+
+# 2
+'''
+a = int(input("a = "))
+b = int(input("b = "))
+print(a * b)
+'''
+
+from tkinter import Y
+
+
+a = 5
+b = 6
+
+a = a + b
+b = a - b
+a = a - b
+
+# a, b = b, a
+
+print(a)
+print(b)
